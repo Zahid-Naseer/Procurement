8fe3816f9024407886d575ba7b3c8ddad9fb06fa
Zahid-Naseer <120409709+Zahid-Naseer@users.noreply.github.com>
2024-06-06 13:30:15 +0500

    Update settings.py

--- procurement/settings.py
+++ procurement/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-q^+*^fl%2ouk7=5$4)mp%c%#dxmz41%+qh2)kg3^*^)l(l!c2m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -132,3 +132,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles_build', 'static')
